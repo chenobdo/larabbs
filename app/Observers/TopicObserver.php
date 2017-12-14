12c615a3da879cdba668107eff6b1ac028ad1cbb
iej <iej@123.com>
2017-12-14 14:53:09 +0800

    test

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -24,5 +24,10 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
 
         $topic->excerpt = make_excerpt($topic->body);
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if (!$topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
