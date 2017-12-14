97dadcdda309e171be581435e9a6ac2921dd7e72
iej <iej@123.com>
2017-12-14 10:50:48 +0800

    test

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -38,7 +38,7 @@
                                   style="margin-right:8px; margin-top:-5px;">
                                 <img src="{{ Auth::user()->avatar }}"
                                      class="img-responsive img-circle"
-                                     width="30px" height="30px">
+                                     style="height: 30px;width: 30px">
                             </span>
                                 {{ Auth::user()->name }} <span
                                         class="caret"></span>
