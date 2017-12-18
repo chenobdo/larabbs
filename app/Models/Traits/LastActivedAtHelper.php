a45fdb997568ab9ba9166e9ba4279031f1c8cb57
iej <iej@123.com>
2017-12-18 11:00:16 +0800

    test

--- app/Models/Traits/LastActivedAtHelper.php
+++ app/Models/Traits/LastActivedAtHelper.php
@@ -30,4 +30,31 @@ trait LastActivedAtHelper
         // 数据写入 Redis ，字段已存在会被更新
         Redis::hSet($hash, $field, $now);
     }
+
+    public function syncUserActivedAt()
+    {
+        // 获取昨天的日期，格式如：2017-10-21
+        $yestoday_date = Carbon::now()->subDay()->toDateString();
+
+        // Redis 哈希表的命名，如：larabbs_last_actived_at_2017-10-21
+        $hash = $this->hash_prefix . $yestoday_date;
+
+        // 从 Redis 中获取所有哈希表里的数据
+        $dates = Redis::hGetAll($hash);
+
+        // 遍历，并同步到数据库中
+        foreach ($dates as $user_id => $actived_at) {
+            // 会将 `user_1` 转换为 1
+            $user_id = str_replace($this->field_prefix, '', $user_id);
+
+            // 只有当用户存在时才更新到数据库中
+            if ($user = $this->find($user_id)) {
+                $user->last_actived_at = $actived_at;
+                $user->save();
+            }
+        }
+
+        // 以数据库为中心的存储，既已同步，即可删除
+        Redis::del($hash);
+    }
 }
