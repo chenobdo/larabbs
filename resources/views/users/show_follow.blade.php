f3a3705ee452c82a9a60e3e3330db211c77874af
Gabriel <obdo.briel@gmail.com>
2017-12-18 16:01:44 +0800

    test

--- resources/views/users/show_follow.blade.php
+++ resources/views/users/show_follow.blade.php
@@ -8,7 +8,7 @@
         <ul class="users">
             @foreach ($users as $user)
                 <li>
-                    <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
+                    <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="gravatar"/>
                     <a href="{{ route('users.show', $user->id )}}" class="username">{{ $user->name }}</a>
                 </li>
             @endforeach
