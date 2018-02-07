169e586295b53f0940862269ad70e5b2c9f115a1
iej <iej@123.com>
2018-02-07 16:14:17 +0800

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -6,44 +6,61 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\UserRequest;
 use App\Transformers\UserTransformer;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
     public function store(UserRequest $request)
     {
-	$verifyData = \Cache::get($request->verification_key);
-	
-	if (!$verifyData) {
-	    return $this->response->error('验证码已失效', 422);
-	}
-
-	if (!hash_equals($verifyData['code'], $request->verification_code)) {
+        $verifyData = \Cache::get($request->verification_key);
+        
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
+        }
+        
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
             // 返回401
             return $this->response->errorUnauthorized('验证码错误');
         }
-
+        
         $user = User::create([
-            'name' => $request->name,
-            'phone' => $verifyData['phone'],
+            'name'     => $request->name,
+            'phone'    => $verifyData['phone'],
             'password' => bcrypt($request->password),
         ]);
-
+        
         // 清除验证码缓存
         \Cache::forget($request->verification_key);
-
+        
         return $this->response->created();
     }
-
+    
     public function me()
     {
-	$user = $this->user();
-
-	return $this->response->item($user, new UserTransformer())
-	    ->setMeta([
-      	        'access_token' => \Auth::guard('api')->fromUser($user),
-                'token_type' => 'Bearer',
-                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        $user = $this->user();
+        
+        return $this->response->item($user, new UserTransformer())
+            ->setMeta([
+                'access_token' => \Auth::guard('api')->fromUser($user),
+                'token_type'   => 'Bearer',
+                'expires_in'   => \Auth::guard('api')->factory()->getTTL() * 60
             ])
             ->setStatusCode(201);
+    }
+    
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+        
+        $attributes = $request->all();
+        
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+            
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+        
+        return $this->response->item($user, new UserTransformer());
     }
 }
